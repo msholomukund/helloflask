95b1fec3579061cdb3804434f853f14d7046c7dd
mkmkay <msholomukund@gmail.com>
2019-06-27 18:03:36 +0530

    all good

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from fastai import *
+from fastai.vision import *
+path = Path()
+print(path)
+# data = ImageDataBunch.from_folder()
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Hello, World !"
+    return "Hello, World, this is going to be cool !!"
 if __name__ == "__main__" :
     app.run()
 #just to test a change
